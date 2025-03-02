cb9095425e9e4981a1989a1f1a8d9531b3303071
Ehsan <ehsan.sasanian@gmail.com>
2025-03-02 22:44:19 +0100

    Change AppController to Kotlin code

--- src/main/kotlin/com/prewave/demo/web/AppController.kt
+++ src/main/kotlin/com/prewave/demo/web/AppController.kt
@@ -1,14 +1,13 @@
-package com.prewave.demo.web;
+package com.prewave.demo.web
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-public class AppController {
+class AppController {
+
     @GetMapping
-    public String index() {
-        return "Hello Prewave :)";
-    }
+    fun index(): String = "Hello Prewave :)"
 }
